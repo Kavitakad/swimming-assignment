966d4dc77af1efcefeefd5d0102c8e896488cfac
kavita <kavita1.kad@gmail.com>
2022-05-20 12:34:07 +0530

    swimming assignment

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductComponent } from './component/product/product.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductComponent,
+   
+   
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    
+
+
+
+  
+
+  ],
+  exports: [],
+ 
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
